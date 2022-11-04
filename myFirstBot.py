d4ef619d1a6f8e7b8e8d7b3b64b5ca7e6556aced
Atashka0 <ilyassskb@gmail.com>
2022-11-04 23:07:46 +0600

    first prototype

---
+++ myFirstBot.py
@@ -0,0 +1,37 @@
+import telebot
+from telebot import types
+
+bot = telebot.TeleBot('5666074559:AAFM5JjdBJttmyBcTzZp0fUz-66HBtATrQ4')
+
+@bot.message_handler(commands = ['start'])
+def commands(message):
+    mess = f'Привет, <b>{message.from_user.first_name}</b>'
+    bot.send_message(message.chat.id, mess, parse_mode = 'html')
+
+@bot.message_handler(commands = ['website'])
+def website(message):
+    markUp = types.InlineKeyboardMarkup()
+    markUp.add(types.InlineKeyboardButton("Visit my website", url = "https://atashka0.github.io/"))
+    bot.send_message(message.chat.id, 'Visit my personal website', reply_markup = markUp)
+
+@bot.message_handler(commands = ['help'])
+def website(message):
+    markUp = types.ReplyKeyboardMarkup(resize_keyboard = True, row_width = 1)
+    website = types.KeyboardButton('Website')
+    start = types.KeyboardButton('Start')
+    markUp.add(website, start)
+    bot.send_message(message.chat.id, 'Choose what u need:', reply_markup = markUp)
+
+@bot.message_handler()
+def get_user_text(message):
+    if message.text == "Hello":
+        bot.send_message(message.chat.id, "HiHi!", parse_mode = "html")
+    elif message.text == "id":
+        bot.send_message(message.chat.id, f"Your ID: {message.from_user.id}", parse_mode = "html")
+    elif message.text == "photo":
+        photo = open('sassy_couple.png', 'rb')
+        bot.send_photo(message.chat.id, photo)
+    else:
+        bot.send_message(message.chat.id, "I can't get u, sry", parse_mode = 'html')
+
+bot.polling(none_stop=True)
